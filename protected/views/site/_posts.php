e414e767b049ade77617a868c0be613dd0853b9d
massilva <massilva@localhost>
2013-10-05 23:16:11 -0300

    Reestruturando a view MAIN do tema para mudar o conteudo.

---
+++ protected/views/site/_posts.php
@@ -0,0 +1,196 @@
+<div class="container-960 content-page-home">
+    <div class="grid_12 container_12 alpha omega">
+        <div class="grid_8 alpha">
+            <div class="posts">
+                <h1 class="underline-title">Latest <span>posts</span></h1>
+                <div class="underline-bg">
+                    <div class="underline template-based-element-background-color">
+                    </div>
+                </div>
+                <div class="posts-container">
+                    <div class="post-container">
+                        <div class="left-content">
+                            <?php
+//                                    <img alt="post-img" class="post-img" src="themes/clubix-v1/img/pic-1-image.jpg">
+                            echo CHtml::image(Yii::app()->theme->baseUrl.'/img/pic-1-image.jpg',"post-img",array("class"=>"post-img"));
+                            ?>
+                            <h1 class="title-tags">tags</h1>
+                            <span class="tag"><a class="template-based-element-color-hover" href="#">web design</a></span> 
+                            <span class="tag"><a class="template-based-element-color-hover" href="#">life style</a></span>
+                            <span class="tag"><a class="template-based-element-color-hover" href="#">another</a></span>
+                            <span class="tag"><a class="template-based-element-color-hover" href="#">god</a></span>
+                            <span class="tag"><a class="template-based-element-color-hover" href="#">trance music</a></span>
+                        </div>
+                        <div class="right-content">
+                            <a class="title-post" href="blog-page-single.html">Here come
+                                bloag title lorem ipsum dolor ? Dino ninoou can't take this
+                                test because dorms afnt</a>
+                            <div class="clear"></div>
+                            <div class="info-post">
+                                <ul>
+                                    <li>
+                                        <?php
+//                                                <img alt="user" src="themes/clubix-v1/img/user-post-icon.png">
+                                        echo CHtml::image(Yii::app()->theme->baseUrl.'/img/user-post-icon.png',"user",array());
+                                        ?>
+                                        <p><a class="template-based-element-color-hover" href="#">Ses Vasile Daniel</a> in <a class="template-based-element-color-hover" href="#">developing</a></p>
+                                    </li>
+                                    <li>
+                                        <?php
+//                                                <img alt="comment" src="themes/clubix-v1/img/comments-post-icon.png">
+                                        echo CHtml::image(Yii::app()->theme->baseUrl.'/img/comments-post-icon.png',"comentario",array());
+                                        ?>
+                                        <p><a class="template-based-element-color-hover" href="blog-page-single.html#comments">100 comments</a></p>
+                                    </li>
+                                    <li>
+                                        <?php
+//                                                <img alt="time" src="themes/clubix-v1/img/time-post-icon.png">
+                                        echo CHtml::image(Yii::app()->theme->baseUrl.'/img/time-post-icon.png',"time",array());
+                                        ?>
+                                        <p><a class="template-based-element-color-hover" href="blog-page-single.html">21 Mai 2013</a></p>
+                                    </li>
+                                </ul>
+                            </div>
+                            <p class="content-post-text">Morbi gravida, ligula eget lacinia
+                                auctor, mi quam vestibulum dolor, in iaculis ante nulla eu
+                                nisi. Integer molestie commodo euismod. Morbi pellentesque
+                                turpis diam, ac interdum erat rutrum et. Aliquam
+                                dapibus</p>
+                            <a class="read-more template-based-element-background-color" href="blog-page-single.html">Read more</a>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+                <h1 class="underline-title">Latest <span>events</span></h1>
+                <div class="underline-bg">
+                    <div class="underline template-based-element-background-color">
+                    </div>
+                </div>
+            </div>
+            <ul class="latest-events home-latest-events">
+                <li class="home-latest-events-li">
+                    <div class="flipping-albums-events container-flip-caption">
+                        <?php
+//                                <img alt="latest-event" class="img-latest-event template-based-element-border-color" src="themes/clubix-v1/img/tournament-1-img.jpg">
+                            echo CHtml::image(Yii::app()->theme->baseUrl.'/img/tournament-1-img.jpg',"latest-event",array("class"=>"img-latest-event template-based-element-border-color"));
+                        ?>
+                        <div class="black-sec-tornament">
+                            <h1 class="title-latst-h1"><a href="#">Mamaia beach tornament</a></h1>
+                            <div class="underline-title-latest-events"></div>
+                            <ul class="info-latest-events">
+                                <li>
+                                    <?php
+//                                            <img alt="place" src="themes/clubix-v1/img/place-icon-2.png">
+                                    echo CHtml::image(Yii::app()->theme->baseUrl.'/img/place-icon-2.png',"place",array());
+                                    ?>
+                                    <p>Vama Veche, Romania</p>
+                                </li>
+                                <li>
+                                    <?php
+//                                            <img alt="date" src="themes/clubix-v1/img/date-icon.png">
+                                    echo CHtml::image(Yii::app()->theme->baseUrl.'/img/date-icon.png',"data",array());
+                                    ?>
+                                    <p>19/12/2013</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="back caption">
+                            <?php
+//                                    <img alt="latest-event" class="img-latest-event template-based-element-border-color" src="themes/clubix-v1/img/tournament-1-img.jpg">
+                            echo CHtml::image(Yii::app()->theme->baseUrl.'/img/tournament-1-img.jpg',"latest-event",array("class"=>"img-latest-event template-based-element-border-color"));
+                            ?>
+                            <div class="bg-black-flip">
+                                <h1 class="title-flipp">Mamaia beach tornament</h1>
+                                <div class="under-line-flipp"></div>
+                                <div class="time-latest-events-flipp">
+                                    10 / NOVEMBER / 2013
+                                </div>
+                                <p class="description-tournament">Mauris iaculis porttitor
+                                    posuere. Praesent id metus massa, ut blandit odio. Proin quis
+                                    tortor orci. Etiam at risus et justo dignis. Dasmac dracer
+                                    carades dacer.</p><a class="more-info-flipp template-based-element-background-color" href="#">Inscrever-me</a>
+                                <ul class="info-latest-events">
+                                    <li>
+                                        <?php
+//                                            <img alt="place" src="themes/clubix-v1/img/place-icon-2.png">
+                                        echo CHtml::image(Yii::app()->theme->baseUrl.'/img/place-icon-2.png',"place",array());
+                                        ?>
+                                        <p>Vama Veche, Romania</p>
+                                    </li>
+                                    <li>
+                                        <?php
+//                                                <img alt="date" src="themes/clubix-v1/img/time-icon-2.png">
+                                        echo CHtml::image(Yii::app()->theme->baseUrl.'/img/time-icon-2.png',"data",array());
+                                        ?>
+                                        <p>11:34 PM - 01:32 AM</p>
+                                    </li>
+                                </ul>
+                            </div><span class="clear"></span>
+                        </div>
+                    </div>
+                </li>
+                <li class="home-latest-events-li">
+                    <div class="flipping-albums-events container-flip-caption">
+                        <?php
+//                                <img alt="latest-event" class="img-latest-event template-based-element-border-color" src="themes/clubix-v1/img/tournament-6-img.jpg">
+                        echo CHtml::image(Yii::app()->theme->baseUrl.'/img/tournament-6-img.jpg',"latest-event",array("class"=>"img-latest-event template-border-color"));
+                        ?>
+                        <div class="black-sec-tornament">
+                            <h1 class="title-latst-h1"><a href="#">Mamaia beach
+                                    tornament</a></h1>
+                            <div class="underline-title-latest-events"></div>
+                            <ul class="info-latest-events">
+                                <li>
+                                    <?php
+//                                            <img alt="place" src="themes/clubix-v1/img/place-icon-2.png">
+                                    echo CHtml::image(Yii::app()->theme->baseUrl.'/img/place-icon-2.png',"place",array());
+                                    ?>
+                                    <p>Vama Veche, Romania</p>
+                                </li>
+                                <li>
+                                    <?php
+//                                                <img alt="date" src="themes/clubix-v1/img/date-icon.png">
+                                    echo CHtml::image(Yii::app()->theme->baseUrl.'/img/date-icon.png',"data",array());
+                                    ?>
+                                    <p>19/12/2013</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="back caption">
+                            <img alt="latest-event" class="img-latest-event template-based-element-border-color" src="themes/clubix-v1/img/tournament-6-img.jpg">
+                            <div class="bg-black-flip">
+                                <h1 class="title-flipp">Mamaia beach tornament</h1>
+                                <div class="under-line-flipp"></div>
+                                <div class="time-latest-events-flipp">
+                                    10 / NOVEMBER / 2013
+                                </div>
+                                <p class="description-tournament">Mauris iaculis porttitor
+                                    posuere. Praesent id metus massa, ut blandit odio. Proin quis
+                                    tortor orci. Etiam at risus et justo dignis. Dasmac dracer
+                                    carades dacer.</p><a class="more-info-flipp template-based-element-background-color" href="#">Inscrever-me</a>
+                                <ul class="info-latest-events">
+                                    <li>
+                                        <?php
+//                                                <img alt="place" src="themes/clubix-v1/img/place-icon-2.png">
+                                        echo CHtml::image(Yii::app()->theme->baseUrl.'/img/place-icon-2.png',"place",array());
+                                        ?>
+                                        <p>Vama Veche, Romania</p>
+                                    </li>
+                                    <li>
+                                        <?php
+//                                                <img alt="date" src="themes/clubix-v1/img/time-icon-2.png">
+                                        echo CHtml::image(Yii::app()->theme->baseUrl.'/img/time-icon-2.png',"time",array());
+                                        ?>
+                                        <p>11:34 PM - 01:32 AM</p>
+                                    </li>
+                                </ul>
+                            </div><span class="clear"></span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <div class="clear"></div>
+        </div>
+    </div>
+    <div class="clear"></div>
+</div> 
